5bc63913fb4543605034f8bb4d2bfd57c1321cd4
Vanessa Barone <vanbarone.milani@gmail.com>
2022-03-31 17:38:49 -0300

    Criando operacao de instanciacao

--- src/main/java/com/vanbarone/cursomc/CursomcApplication.java
+++ src/main/java/com/vanbarone/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.vanbarone.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vanbarone.cursomc.domain.Categoria;
+import com.vanbarone.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
