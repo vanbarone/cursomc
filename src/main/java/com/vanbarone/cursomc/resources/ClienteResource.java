7a1a9102cf54928cce87a150d9a2e5fbf76fd2cc
Vanessa Barone <vanbarone.milani@gmail.com>
2022-03-31 21:28:26 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/vanbarone/cursomc/resources/ClienteResource.java
+++ src/main/java/com/vanbarone/cursomc/resources/ClienteResource.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vanbarone.cursomc.domain.Categoria;
 import com.vanbarone.cursomc.domain.Cliente;
 import com.vanbarone.cursomc.services.ClienteService;
 
@@ -24,6 +26,13 @@ public class ClienteResource {
 		List<Cliente> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
--- src/main/java/com/vanbarone/cursomc/services/ClienteService.java
+++ src/main/java/com/vanbarone/cursomc/services/ClienteService.java
@@ -1,20 +1,30 @@
 package com.vanbarone.cursomc.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vanbarone.cursomc.domain.Cliente;
 import com.vanbarone.cursomc.repositories.ClienteRepository;
+import com.vanbarone.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	public List<Cliente> findAll() {
 		return repo.findAll();
+	}
+
+	public Cliente buscar(Integer id) {
+
+		Optional<Cliente> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName(), null));
 	}
 }
